b329be871c19fe605cc6b814f9bec84a44d50810
mashin2002 <mashin0128@gmail.com>
2024-10-15 15:45:14 +0900

    feat: add Dockerfile & Eureka Client setting

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Dockerfile
+FROM openjdk:17-jdk
+
+VOLUME /tmp
+
+ARG JAR_FILE=./build/libs/Lecture-0.0.1-SNAPSHOT.jar
+
+COPY ${JAR_FILE} app.jar
+
+ENTRYPOINT [ "java", "-jar", "/app.jar" ]
+
+
+
+
--- src/main/java/com/allcear/alclectureapi/AlcLectureApiApplication.java
+++ src/main/java/com/allcear/alclectureapi/AlcLectureApiApplication.java
@@ -2,8 +2,10 @@ package com.allcear.alclectureapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AlcLectureApiApplication {
 
     public static void main(String[] args) {
