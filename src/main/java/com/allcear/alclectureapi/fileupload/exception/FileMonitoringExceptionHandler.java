c2dacd22c12d2b33dd8931bf48f3e8cddc286c73
me_nyong <80142915+MinCodeHub@users.noreply.github.com>
2024-10-23 00:10:44 +0900

    feat: add exception FileUploadExceptionHandler #5

---
+++ src/main/java/com/allcear/alclectureapi/fileupload/exception/FileMonitoringExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.allcear.alclectureapi.fileupload.exception;
+
+
+import com.allcear.alclectureapi.common.apiPayload.exception.GeneralException;
+import com.allcear.alclectureapi.fileupload.enums.FileUploadErrorCode;
+
+public class FileMonitoringExceptionHandler extends GeneralException {
+
+    public FileMonitoringExceptionHandler(FileUploadErrorCode errorCode) {
+        super(errorCode);
+    }
+}
